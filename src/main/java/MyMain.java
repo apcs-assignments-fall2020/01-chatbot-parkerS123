778b702451d83ffa5525f5c0d99c11d3e521127d
parkerschwantes <23pschwantes@athenian.org>
2021-09-01 19:17:01 -0700

    first try

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -6,6 +6,26 @@ public class MyMain {
 
         // Add some code here!
 
+        System.out.println("What is your name?"); // name
+        String name = scan.next();
+        System.out.println("Hi, " + name);
+
+        System.out.println("What is your favorite color?"); // color
+        String color = scan.next();
+        System.out.println(color + "is a great color");
+
+        System.out.println("What is your favorite sport?");
+        String sport = scan.next();
+        System.out.println("I like basketball, but " + sport + " is fun too");
+
+        System.out.println("What is your dream location to live?");
+        String location = scan.next();
+        System.out.println(location + " is a great choice");
+
+        System.out.println("What is your favorite food?");
+        String food = scan.next();
+        System.out.println(food + " is yummy");
+
         scan.close();
     }
 }
